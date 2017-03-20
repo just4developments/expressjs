0f02c13497fb07ed1f0da0485ed9651246e297ac
thanhdt-viosoft <thanhdt@viosoft.com>
2017-03-20 17:19:21 +0700

    Init project

--- src/appconfig.js
+++ src/appconfig.js
@@ -1,10 +1,14 @@
 global.appconfig = {
+    name: 'plugin.core',
     listen: 9002,
     db: {
         url: 'mongodb://localhost:27017/core'
     },
-    pubsub: {
-        url: 'amqp://127.0.0.1',
+    services: {
+        manager: 'http://localhost:9599',
+        auth: 'http://localhost:9600',
+        mail: 'http://localhost:9602',
+        log: 'http://localhost:9603'
     },
     app: {
         encrypt: '*(*#$&'
---
+++ src/service/micro.service.js
@@ -0,0 +1,41 @@
+const _ = require('lodash');
+const httpService = require('./http.service');
+
+/************************************
+ ** SERVICE:      microService
+ ** AUTHOR:       Unknown
+ ** CREATED DATE: 2/7/2017, 3:31:14 PM
+ *************************************/
+
+exports = module.exports = {
+    async checkAuthoriz(headers) {
+        return await httpService.head(`${global.appconfig.services.auth}/authoriz`, {
+            headers: headers
+        })        
+    },
+    async updateConfig(auth, data) {
+        const resp = await httpService.put(`${global.appconfig.services.auth}/config`, {
+            headers: {
+                token: auth.rawToken,
+                path: '/config',
+                actions: 'UPDATE'
+            },
+            data: data
+        });
+        return resp;
+    },
+    async getConfig(auth, pluginName) {
+        const resp = await httpService.get(`${global.appconfig.services.auth}/config`, {
+            headers: {
+                token: auth.rawToken,
+                path: '/config',
+                actions: 'GET'
+            },
+            query: {
+                plugin: pluginName
+            }
+        });
+        if (resp.code === 200) return resp.body;
+        throw resp.body;
+    },
+}
